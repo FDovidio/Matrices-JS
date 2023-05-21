11b83a84ab0a7c8aacf8741e11d8c6ce3a1e61e3
Francisco Dovidio <francisco.dovidio+1@gmail.com>
2023-05-21 16:32:25 -0300

    coreccion en alert y validador nombre

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const listaArticulos = [
-  { nombre: "Yerba", id: 1, precio: 1000, stock: 200 },
-  { nombre: "Termo", id: 2, precio: 10000, stock: 70 },
-  { nombre: "Mate", id: 3, precio: 5000, stock: 150 },
+    { nombre: "Yerba", id: 1, precio: 1000, stock: 200 },
+    { nombre: "Termo", id: 2, precio: 10000, stock: 70 },
+    { nombre: "Mate", id: 3, precio: 5000, stock: 150 },
 ];
 
 const listaTienda = `Actualemte tenemos en stock \n
@@ -11,7 +11,7 @@ const listaTienda = `Actualemte tenemos en stock \n
                     0 - Finalizar la compra`;
 
 let eleccionUser = parseInt(
-  prompt(`Indique el numero del articulo que desea comprar :\n ${listaTienda}`)
+    prompt(`Indique el numero del articulo que desea comprar :\n ${listaTienda}`)
 );
 
 const carrito = [];
@@ -19,67 +19,71 @@ const articulosElegidosStock = [];
 let sumaTotal = 0;
 
 function finaliza() {
-  if (sumaTotal !== 0) {
-    const carritoMostrado = carrito.map(function (articulo) {
-      return articulo.nombre;
-    });
-    const carritoString = carritoMostrado.join(" - \n -  ");
-    alert(`Usted llevara ${carritoMostrado}`);
-    let factura = prompt("Necesita factura? Escriba SI o NO").toLowerCase();
-    const fecha = new Date();
-    if (factura == "si" || factura == "sí") {
-      let nombreUser = prompt("Ingresa tu nombre");
-      if (nombreUser !== "") {
-        alert(
-          `${fecha.toLocaleString()}  \n ${nombreUser}: \n ${carritoString} \n Total final: $ ${
-            sumaTotal * 1.21
-          }`
-        );
-      } else {
-        alert("Escriba un nombre");
-        nombreUser = prompt("Ingresa tu nombre");
-      }
+    if (sumaTotal !== 0) {
+        const carritoMostrado = carrito.map(function (articulo) {
+            return articulo.nombre;
+        });
+        const carritoString = carritoMostrado.join("\n - "  );
+        alert(`Usted llevara ${carritoMostrado}`);
+        
+        let factura = prompt("Necesita factura? Escriba SI o NO").toLowerCase();
+        const fecha = new Date();
+        if (factura == "si" || factura == "sí") {
+            let nombreUser = prompt("Ingresa tu nombre");
+            if (nombreUser.length==0){
+                alert("Tiene que escribir su nombre")
+                nombreUser = prompt("Ingresa tu nombre");
+            }
+            if (nombreUser !== "") {
+                alert(
+                    `${fecha.toLocaleString()}  \n ${nombreUser}: \n - ${carritoString} \n Total final: $ ${sumaTotal * 1.21
+                    }`
+                );
+            } else {
+                alert("Escriba un nombre");
+                nombreUser = prompt("Ingresa tu nombre");
+            }
+        } else {
+            alert(
+                `${fecha.toLocaleString()} \n ${nombreUser}: \n -${carritoString} \n Total final: $${sumaTotal} `
+            );
+        }
     } else {
-      alert(
-        `${fecha.toLocaleString()} \n ${nombreUser}: \n ${carritoString} \n Total final: $${sumaTotal} `
-      );
+        alert("No haz agregado nada a tu compra");
     }
-  } else {
-    alert("No haz agregado nada a tu compra");
-  }
 };
 
 while (isNaN(eleccionUser)) {
-  alert("Ingresaste un valor no numerico, reintentalo");
-  eleccionUser = parseInt(
-    prompt(
-      `Indique el numero del articulo que desea comprar :\n ${listaTienda}`
-    )
-  );
+    alert("Ingresaste un valor no numerico, reintentalo");
+    eleccionUser = parseInt(
+        prompt(
+            `Indique el numero del articulo que desea comprar :\n ${listaTienda}`
+        )
+    );
 }
 while (eleccionUser !== 0) {
-  let unidades = parseInt(prompt(`Cuantas unidades desea?`));
-  const articulosElegidos = listaArticulos.find((e) => e.id == eleccionUser);
+    let unidades = parseInt(prompt(`Cuantas unidades desea?`));
+    const articulosElegidos = listaArticulos.find((e) => e.id == eleccionUser);
 
-  articulosElegidosStock.push(articulosElegidos);
-  articulosElegidosStock.forEach((element) => {
-    element.stock = element.stock - unidades;
-  });
+    articulosElegidosStock.push(articulosElegidos);
+    articulosElegidosStock.forEach((element) => {
+        element.stock = element.stock - unidades;
+    });
 
-  const validador = articulosElegidosStock.some((e) => e.stock >= unidades);
-  if (validador) {
-    alert(
-      `Sumamos ${articulosElegidos.nombre} por la cantidad de ${unidades} unidades`
+    const validador = articulosElegidosStock.some((e) => e.stock >= unidades);
+    if (validador) {
+        alert(
+            `Sumamos ${articulosElegidos.nombre} por la cantidad de ${unidades} unidades`
+        );
+        carrito.push(articulosElegidos);
+        sumaTotal += articulosElegidos.precio * unidades;
+    } else {
+        alert(`No tenemos suficiente stock para tu pedido`);
+    }
+    eleccionUser = parseInt(
+        prompt(
+            `Indique el numero del articulo que desea comprar :\n ${listaTienda}`
+        )
     );
-    carrito.push(articulosElegidos);
-    sumaTotal += articulosElegidos.precio * unidades;
-  } else {
-    alert(`No tenemos suficiente stock para tu pedido`);
-  }
-  eleccionUser = parseInt(
-    prompt(
-      `Indique el numero del articulo que desea comprar :\n ${listaTienda}`
-    )
-  );
 }
 finaliza();
